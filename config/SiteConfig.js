bac0718aec6643a4991d8a439b8ef238311f7848
LeKoArts <lekoarts@gmail.com>
2018-06-01 01:02:14 +0200

    Fixes

--- config/SiteConfig.js
+++ config/SiteConfig.js
@@ -2,12 +2,12 @@ module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
   siteTitle: 'Emma', // Navigation and Site Title
   siteTitleAlt: 'Emma - Gatsby Starter Portfolio', // Alternative Site title for SEO
-  siteUrl: 'https://portfolio-emma.netlify.com/', // Domain of your site. No trailing slash!
+  siteUrl: 'https://portfolio-emma.netlify.com', // Domain of your site. No trailing slash!
   siteLanguage: 'en', // Language Tag on <html> element
   siteLogo: '/logos/logo-1024.png', // Used for SEO and manifest
   siteDescription: 'Minimalistic bright portfolio with full-width grid and large images',
   siteFBAppID: '123456789', // Facebook App ID
-  userTwitter: 'emma', // Twitter Username
+  userTwitter: '@emma', // Twitter Username
   // og: Meta Tags
   ogSiteName: 'emma', // Facebook Site Name
   googleAnalyticsID: 'UA-12345689-1',
