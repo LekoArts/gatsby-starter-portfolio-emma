59dc5ffc3ef3812e5285d3d636448aaed19c3292
LekoArts <lekoarts@gmail.com>
2019-03-11 17:30:16 +0100

    add cypress helper commands to hopefully mitigate weird fails on CI

--- cypress/e2e/home.js
+++ cypress/e2e/home.js
@@ -1,6 +1,6 @@
 describe('Home', () => {
   beforeEach(() => {
-    cy.visit('/')
+    cy.visit('/').waitForRouteChange()
   })
   it('navigation renders', () => {
     cy.getByTestId('navigation')
@@ -11,6 +11,7 @@ describe('Home', () => {
   it('link on project item works', () => {
     cy.getByTestId('projectItem-0')
       .click()
+      .waitForRouteChange()
       .getByTestId('project-title')
   })
   it('link to first nav item works', () => {
@@ -21,8 +22,10 @@ describe('Home', () => {
   it('title links to homepage', () => {
     cy.getByTestId('navItem-0')
       .click()
+      .waitForRouteChange()
       .getByTestId('home-title-link')
       .click()
+      .waitForRouteChange()
       .assertRoute('/')
   })
 })
