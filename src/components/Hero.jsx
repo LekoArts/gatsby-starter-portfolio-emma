8e77a5b9267ef9e04c6e6e5ff1edfcb7da42ad14
Panuruj Khambanonda (PK) <pk@panurujk.com>
2019-07-01 11:07:28 -0700

    Fix invalid CSS for responsive hero height (#115)
    The CSS for the hero height was invalid on the mobile screen width.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,10 +7,10 @@ const Hero = styled.section`
   overflow: hidden;
   margin-bottom: ${props => (props.single ? '4rem' : '6rem')};
   @media (max-width: ${props => props.theme.breakpoints.m}), (max-device-width: ${props => props.theme.breakpoints.m}) {
-    ${props => (props.single ? '40vw' : '60vw')};
+    height: ${props => (props.single ? '40vw' : '60vw')};
   }
   @media (max-width: ${props => props.theme.breakpoints.s}), (max-device-width: ${props => props.theme.breakpoints.s}) {
-    ${props => (props.single ? '300px' : '400px')};
+    height: ${props => (props.single ? '300px' : '400px')};
   }
   @media (max-width: ${props => props.theme.breakpoints.xs}),
     (max-device-width: ${props => props.theme.breakpoints.xs}) {
