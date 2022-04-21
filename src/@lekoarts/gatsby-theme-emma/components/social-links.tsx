1cfd8b7e295fe9ea3b0683a2c47364501f7e8212
Mario <mariob360@gmail.com>
2022-04-21 15:25:33 -0700

    fix label on socials

--- src/@lekoarts/gatsby-theme-emma/components/social-links.tsx
+++ src/@lekoarts/gatsby-theme-emma/components/social-links.tsx
@@ -6,13 +6,13 @@ import {TiSocialTwitterCircular, TiSocialLinkedinCircular} from 'react-icons/ti'
 const SocialLinks = () => (
   <>
     {/* TWITTER */}
-    <a href="https://twitter.com/leilamdesigns" target="_blank" rel="noopener noreferrer" aria-label="Dribbble">
+    <a href="https://twitter.com/leilamdesigns" target="_blank" rel="noopener noreferrer" aria-label="Twitter">
       <IconContext.Provider value={{ size: "1.3em" }}>
           <TiSocialTwitterCircular />
       </IconContext.Provider>
     </a>
     {/* LINKEDIN */}
-    <a href="https://www.linkedin.com/in/leilakmeyer/" target="_blank" rel="noopener noreferrer" aria-label="Dribbble">
+    <a href="https://www.linkedin.com/in/leilakmeyer/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
       <IconContext.Provider value={{ size: "1.3em" }}>
           <TiSocialLinkedinCircular />
       </IconContext.Provider>
